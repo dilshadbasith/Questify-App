7b6880090479ef6f0336e404df8d6ad3ccd55908
dilshadbasith <dilshadbasith01@gmail.com>
2023-11-09 15:35:34 -0800

    add back btn

--- Frontend/questify-frontend/src/Components/Profile.jsx
+++ Frontend/questify-frontend/src/Components/Profile.jsx
@@ -3,12 +3,16 @@ import "./css/Profile.css";
 import { Avatar } from "@mui/material";
 import { useSelector } from "react-redux";
 import { selectUser } from "../feature/userSlice";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const user = useSelector(selectUser);
+  const navigate = useNavigate() 
   return (
     <div className="maindiv">
       <div>
+        <ArrowBackIcon className="back-btn" onClick={()=>navigate('/home')}/>
         <Avatar
           sx={{
             width: "100px !important",
