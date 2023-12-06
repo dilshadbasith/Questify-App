f3faa8c5e2c095271dc2edef6b4a8279f8264a75
dilshadbasith <dilshadbasith01@gmail.com>
2023-12-05 16:51:46 -0800

    change in package

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,16 @@
+require("dotenv").config()
 const express= require('express');
 const cors=require('cors')
 const  mongoose = require('mongoose');
 const app= express()
-const port = 3000;
+const port = process.env.PORT;
 const userRoute=require('./routes/userRoute');
 const adminRoute=require('./routes/adminRoute')
 const questionRoute=require('./routes/questionRoute')
 const answerRoute=require('./routes/answerRoute')
 const loginRoute=require('./routes/loginRoute')
-require("dotenv").config()
-mongoose.connect("mongodb://0.0.0.0:27017/backend-project");
+mongoose.connect("mongodb://0.0.0.0:27017/backend-project")
+.then(()=>console.log("connected"))
 
 app.use(cors())
 app.use(express.json())
